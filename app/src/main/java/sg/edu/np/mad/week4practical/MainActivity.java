11c6a3500d9130bec617827f48335054e73c7d35
roofclimb <s10243726@connect.np.edu.sg>
2023-06-30 16:11:50 +0800

    update

--- app/src/main/java/sg/edu/np/mad/week4practical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week4practical/MainActivity.java
@@ -20,7 +20,7 @@ import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
-    String TAG = "RecyclerVieww";
+    String TAG = "RecyclerView";
     ArrayList<myObject> myObject_list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
